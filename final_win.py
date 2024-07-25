912e2382f2ff2ecbf92cb4c27c6cfe6103b8eb43
ScorpioKiller <sasacickan89@gmail.com>
2024-07-25 20:37:24 +0300

    0.0.6

--- final_win.py
+++ final_win.py
@@ -1,1 +1,27 @@
-# напиши тут код третього екрана програми
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QWidget, QVBoxLayout, QLabel
+
+from instr import *
+
+
+class FinalWin(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()  # створюємо та налаштовуємо графічні елементи
+        self.set_appear()  # Встановлює, як виглядатиме вікно (напис, розмір, місце)
+        self.show()  # старт
+
+    def set_appear(self):
+        self.setWindowTitle(txt_finalwin)
+        self.resize(win_width, win_height)
+        self.move(win_x, win_y)
+
+    def initUI(self):
+        self.workh_text = QLabel(txt_workheart)
+        self.index_text = QLabel(txt_index)
+
+        self.layout_line = QVBoxLayout()
+        self.layout_line.addWidget(self.index_text, alignment=Qt.AlignCenter)
+        self.layout_line.addWidget(self.workh_text, alignment=Qt.AlignCenter)
+        self.setLayout(self.layout_line)
+
--- my_app.py
+++ my_app.py
@@ -1,1 +1,39 @@
-# напиши тут код основної програми та першого екрану
+from second_win import *
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QVBoxLayout
+
+
+class MainWin(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()  # створюємо та налаштовуємо графічні елементи
+        self.connects()  # Встановлює зв'язки між елементами
+        self.set_appear()  # Встановлює, як виглядатиме вікно (напис, розмір, місце)
+        self.show()  # старт
+
+    def set_appear(self):
+        self.setWindowTitle(txt_title)
+        self.resize(win_width, win_height)
+        self.move(win_x, win_y)
+
+    def initUI(self):
+        self.btn_next = QPushButton(txt_next)
+        self.hello_text = QLabel(txt_hello)
+        self.instruction = QLabel(txt_instruction)
+
+        self.layout_line = QVBoxLayout()
+        self.layout_line.addWidget(self.hello_text, alignment=Qt.AlignLeft)
+        self.layout_line.addWidget(self.instruction, alignment=Qt.AlignLeft)
+        self.layout_line.addWidget(self.btn_next, alignment=Qt.AlignCenter)
+        self.setLayout(self.layout_line)
+
+    def next_click(self):
+        self.tw = TestWin()
+        self.hide()
+
+    def connects(self):
+        self.btn_next.clicked.connect(self.next_click)
+
+
+app = QApplication([])
+mw = MainWin()
+app.exec_()
